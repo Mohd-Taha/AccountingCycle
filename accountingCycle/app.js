96cf3eefa61ca0b515d12c41044c2592238bef3f
Mohammad Taha <taha2000mohammad@gmail.com>
2022-06-13 19:59:35 +0500

    Shukr Alhamd

--- accountingCycle/app.js
+++ accountingCycle/app.js
@@ -1,82 +1,115 @@
-const express = require('express');
-const mysql = require('mysql');
-const path = require('path');
-const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000;
-const static_path = path.join(__dirname, '/views');
-// console.log(path.join(__dirname))
+// const express = require('express');
+// const mysql = require('mysql');
+// const path = require('path');
+// const bodyParser = require('body-parser');
+// const port = process.env.PORT || 3000;
+// const static_path = path.join(__dirname, '/views');
+// // console.log(path.join(__dirname))
 
-const app = express()
+// const app = express()
+// const router = express.Router()
 
-app.use(express.static(static_path));
-// app.use(express.urlencoded({ extended: false }));
-app.use(express.json);
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(static_path));
+// // app.use(express.urlencoded({ extended: false }));
+// // app.use(express.json);
+// // app.use(bodyParser.urlencoded({ extended: false }));
 
-//Creating Connection
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: ''
-});
+// //Creating Connection
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: ''
+// });
 
-//Connect to MySQL
-db.connect(err => {
-    if (err) {
-        throw err;
-    };
-    console.log('MySQL Connected');
-});
+// //Connect to MySQL
+// db.connect(err => {
+//     if (err) {
+//         throw err;
+//     };
+//     console.log('MySQL Connected');
+// });
 
-app.get('/', (req, res) => {
-    res.render(path.join(static_path, '/index.html'))
-})
+// //Create Database
+// app.get('/createdatabase', (req, res) => {
+//     let sql = 'CREATE DATABASE financialAccounting';
+//     db.query(sql, err => {
+//         if (err) {
+//             throw err;
+//         }
+//         res.send('Database Created');
+//     });
+// });
 
-app.get('/addTransaction', (req, res) => {
-    res.render(path.join(static_path, '/public/manualAdd.html'))
-})
+// //Create Table
+// app.get('/createTables', (req, res) => {
+//     let sql = 'CREATE TABLE accounts (id int AUTO_INCREMENT, name VARCHAR(255), type VARCHAR(20), contact VARCHAR(20), email VARCHAR(255), address VARCHAR(255), about VARCHAR(255), PRIMARY KEY(id))'
+//     db.query(sql, err => {
+//         if (err) {
+//             throw err;
+//         }
+//         res.send('Accounts Table Created');
+//     });
+//     let sql2 = 'CREATE TABLE transactions (id int AUTO_INCREMENT, transactionDate DATE, debitAccounts VARCHAR(255), debitValues VARCHAR(255), creditAccounts VARCHAR(255), creditValues VARCHAR(255), PRIMARY KEY(id))'
+//     db.query(sql2, err => {
+//         if (err) {
+//             throw err;
+//         }
+//         res.send('Transactions Table Created');
+//     });
+// })
 
-app.get('/public/manualAdd.html', (req, res) => {
-    res.sendFile(path.join(static_path, '/public/manualAdd.html'))
-})
+// router.get('/', (req, res) => {
+//     console.log("svsdvsdv", db);
+//     res.send("<h1>Hello And Welcome</h1>");
+//     // res.sendFile(path.join(static_path, '/index.html'))
 
-app.get('/createAccountsRecords', (req, res) => {
-    let rec = { name: 'Office Rent', type: 'expense', contact: '+92-123-34567890', email: 'someone@abc.com', address: '1st Floor, ABC Building, Karachi', about: 'Have to Pay Amount till 5th of Every Month' }
-    let sql = 'INSERT INTO accounts set ?'
-    db.query(sql, rec, err => {
-        if (err) {
-            throw err;
-        }
-        res.send('Record Inserted');
-    });
-})
+// })
+
+// app.get('/addTransaction', (req, res) => {
+//     res.sendFile(path.join(static_path, '/public/manualAdd.html'))
+// })
+
+// app.get('/public/manualAdd.html', (req, res) => {
+//     res.sendFile(path.join(static_path, '/public/manualAdd.html'))
+// })
+
+// app.get('/createAccountsRecords', (req, res) => {
+//     let rec = { name: 'Office Rent', type: 'expense', contact: '+92-123-34567890', email: 'someone@abc.com', address: '1st Floor, ABC Building, Karachi', about: 'Have to Pay Amount till 5th of Every Month' }
+//     let sql = 'INSERT INTO accounts set ?'
+//     db.query(sql, rec, err => {
+//         if (err) {
+//             throw err;
+//         }
+//         res.send('Record Inserted');
+//     });
+// })
 
 
-app.post('/public/ledgerCreate', async(req, res) => {
-    try {
-        accName = req.body.name;
-        accType = req.body.type;
-        accContact = req.body.contact;
-        accEmail = req.body.email;
-        accAdd = req.body.address;
-        accAbout = req.body.about;
-        console.log(req.body)
+// app.post('/public/ledgerCreate', async(req, res) => {
+//     try {
+//         accName = req.body.name;
+//         accType = req.body.type;
+//         accContact = req.body.contact;
+//         accEmail = req.body.email;
+//         accAdd = req.body.address;
+//         accAbout = req.body.about;
+//         console.log(req.body)
 
-        let rec = { name: accName, type: accType, contact: accContact, email: accEmail, address: accAdd, about: accAbout }
-        let sql = 'INSERT INTO accounts set ?'
-        db.query(sql, rec, err => {
-            if (err) {
-                throw err;
-            }
-            res.send('Record Inserted');
-        });
-        res.redirect('/')
-    } catch (err) {
-        res.sendStatus(400).send(err);
-    }
-})
+//         let rec = { name: accName, type: accType, contact: accContact, email: accEmail, address: accAdd, about: accAbout }
+//         let sql = 'INSERT INTO accounts set ?'
+//         db.query(sql, rec, err => {
+//             if (err) {
+//                 throw err;
+//             }
+//             res.send('Record Inserted');
+//         });
+//         res.redirect('/')
+//     } catch (err) {
+//         res.sendStatus(400).send(err);
+//     }
+// })
 
-app.listen(port, () => {
-    console.log(`Server Started On Port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server Started On Port ${port}`);
+// });
---
+++ accountingCycle/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const path = require('path');
+
+const app = express();
+const static_path = path.join(__dirname, '/views');
+// app.use(express.static_path);
+app.use('/', express.static('views'));
+
+app.listen(port, () => {
+    console.log(`Server Started On Port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    console.log("Index Page Route");
+    res.sendFile('./index.html', { root: static_path });
+})
+
+app.get('/addTransaction', (req, res) => {
+    console.log("Manual Add Transaction Page Route");
+    res.sendFile('./public/manualAdd.html', { root: static_path });
+})
+
+app.get('/accounts', (req, res) => {
+    console.log("Show Accounts Page Route");
+    res.sendFile('./public/ledger.html', { root: static_path });
+})
+
+app.get('/journal', (req, res) => {
+    console.log("General Journal Page Route");
+    res.sendFile('./public/generalJournal', { root: static_path });
+})
+
+app.get('/unAdjustedTrial', (req, res) => {
+    console.log("Un Adjusted TrialBalance Page Route");
+    res.sendFile('./public/trialBalance.html', { root: static_path });
+})
+
+app.get('/accountLedger/:id', (req, res) => {
+    console.log("Only Account Page Route");
+    res.sendFile('./public/accounts.html', { root: static_path });
+})
